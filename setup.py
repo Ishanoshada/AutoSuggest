ef5154a6618f57187c82ec98cd18ac2039f2fa5e
Ishan Oshada <63636533+Ishanoshada@users.noreply.github.com>
2024-10-14 17:13:30 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='autosuggest',
+    version='1.1.0',
+    packages=find_packages(),
+    description='A cross-platform auto-suggest input package with enhanced features',
+    author='Ishan OShada',
+    author_email='ic31908@gmail.com',
+    url='https://github.com/ishanoshada/autosuggest',  # Update with your actual GitHub repo
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
